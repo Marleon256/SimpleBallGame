a989e7d7d435071ce50467ba45491320d6b7b920
Marleon256 <43805057+Marleon256@users.noreply.github.com>
2022-06-12 17:13:55 +0500

    Another stop rate change

--- Scripts/MainCharacter.gd
+++ Scripts/MainCharacter.gd
@@ -3,7 +3,7 @@ extends KinematicBody
 var _score := 0
 var _velocity := Vector3(0, 0, 0)
 const SPEED := 5
-const STOP_RATE := 0.05
+const STOP_RATE := 0.03
 const ROTATION_SPEED := deg2rad(5)
 
 func _ready():
