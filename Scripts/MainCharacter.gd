86c43e9fcde146c67d1601cd06820d691e799571
Marleon256 <43805057+Marleon256@users.noreply.github.com>
2022-06-18 20:46:18 +0500

    Changed game parameters

--- Scripts/MainCharacter.gd
+++ Scripts/MainCharacter.gd
@@ -2,9 +2,9 @@ extends KinematicBody
 
 var _score := 0
 var _velocity := Vector3(0, 0, 0)
-const SPEED := 5
-const STOP_RATE := 0.03
-const ROTATION_SPEED := deg2rad(5)
+const SPEED := 12
+const STOP_RATE := 0.01
+const ROTATION_SPEED := deg2rad(9)
 
 func _ready():
 	pass
